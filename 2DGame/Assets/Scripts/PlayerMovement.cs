5a0658bd71d3d7fe33af3b85faab51edc9b211be
BenjaminHu123 <43998171+BenjaminHu123@users.noreply.github.com>
2019-03-10 12:06:45 -0400

    player movement
    z is atk
    space is jump
    arrow keys to move

---
+++ 2DGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Rigidbody2D myRigidbody;
+
+    [SerializeField]
+    private float movementSpeed;
+    private bool facingRight;
+    private Animator myAnimator;
+    private bool attack;
+    [SerializeField]
+    private Transform[] groundPoints;
+    [SerializeField]
+    private float groundRadius;
+    [SerializeField]
+    private LayerMask whatIsGround;
+    private bool isGrounded;
+    private bool jump;
+    [SerializeField]
+    private float jumpForce;
+    [SerializeField]
+    private bool airControl;
+
+	// Use this for initialization
+	void Start () {
+        facingRight = true;
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+	}
+
+    // Update is called once per frame
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    void FixedUpdate () {
+        float horizontal = Input.GetAxis("Horizontal");
+        isGrounded = IsGrounded();
+        HandleMovement(horizontal);
+        Flip(horizontal);
+        HandleAttacks();
+        HandleLayers();
+        ResetValues();
+    }
+
+    private void HandleMovement(float horizontal)
+    {
+        if (myRigidbody.velocity.y < 0) {
+            myAnimator.SetBool("land", true);
+        }
+        if ((isGrounded || airControl))
+        {
+            myRigidbody.velocity = new Vector2(horizontal * movementSpeed, myRigidbody.velocity.y);
+            myAnimator.SetFloat("speed", Mathf.Abs(horizontal));
+        }
+        if (isGrounded && jump) {
+            isGrounded = false;
+            myRigidbody.AddForce(new Vector2(0, jumpForce));
+            myAnimator.SetTrigger("jump");
+        }
+    }
+
+    private void HandleAttacks() {
+        if (attack) {
+            myAnimator.SetTrigger("attack");
+        }
+    }
+
+    private void HandleInput() {
+        if (Input.GetKeyDown(KeyCode.Z)) {
+            attack = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            jump = true;
+        }
+    }
+
+    private void Flip(float horizontal) {
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight) {
+            facingRight = !facingRight;
+            Vector2 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+
+    private void ResetValues()
+    {
+        attack = false;
+        jump = false;
+    }
+
+    private bool IsGrounded() {
+        if (myRigidbody.velocity.y <= 0) {
+            foreach (Transform point in groundPoints) {
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position, groundRadius, whatIsGround);
+                for (int i = 0; i < colliders.Length; i++) {
+                    if (colliders[i].gameObject != gameObject)
+                    {
+                        myAnimator.SetTrigger("jump");
+                        myAnimator.SetBool("land", false);
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private void HandleLayers() {
+        if (!isGrounded)
+        {
+            myAnimator.SetLayerWeight(1, 1);
+        }
+        else
+            myAnimator.SetLayerWeight(1, 0);
+
+    }
+}
